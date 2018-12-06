00cafbe96faa2632c948cb0cfa7f3f87dbb83fc5
oranat <oranat.kan@gmail.com>
2018-12-06 11:03:48 +0700

    Initial commit

---
+++ app/src/main/java/akkaudom/oranat/th/ac/su/reg/homecare/MainActivity.java
@@ -0,0 +1,13 @@
+package akkaudom.oranat.th.ac.su.reg.homecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
